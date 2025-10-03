68610a68f42d48ab9a6731a0053faf8c4a1603fe
hiteshshimpi55 <shimpihitesh2@gmail.com>
2025-10-03 21:29:45 +0530

    feat(stripe): Remove unused files

--- migrations/postgres/V3_add_sync_config_to_connections.down.sql
+++
@@ -1,3 +0,0 @@
--- Remove sync_config column from connections table
-ALTER TABLE connections DROP COLUMN sync_config;
-
--- migrations/postgres/V3_add_sync_config_to_connections.up.sql
+++
@@ -1,3 +0,0 @@
--- Add sync_config column to connections table
-ALTER TABLE connections ADD COLUMN sync_config JSONB;
-
