b51467f08da440c91d479bf9744474a6bdc3a0f0
omkar sonawane <omkarsonawane622@gmail.com>
2025-10-29 15:22:28 +0530

    fix(config): Load environment variables without specifying a file path for improved configuration handling

--- internal/config/config.go
+++ internal/config/config.go
@@ -246,7 +246,7 @@ func NewConfig() (*Configuration, error) {
 	v := viper.New()
 
 	// Step 1: Load `.env` if it exists
-	_ = godotenv.Load("")
+	_ = godotenv.Load()
 
 	// Step 2: Initialize Viper
 	v.SetConfigName("config")
