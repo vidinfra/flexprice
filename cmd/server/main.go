9e314dbb756ac5ea7f4f8e4215a4085ac9cb1128
hiteshshimpi55 <shimpihitesh2@gmail.com>
2025-11-05 16:05:06 +0530

    fix(event-ingestion): skip pushing events to the post-processing topic for non-v1 pipeline tenants.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -516,7 +516,7 @@ func registerRouterHandlers(
 
 	// Only register processing handlers when needed
 	if includeProcessingHandlers {
-		// Register post-processing handlers
+		// Register handlers
 		eventConsumptionSvc.RegisterHandler(router, cfg)
 		eventConsumptionSvc.RegisterHandlerLazy(router, cfg)
 		eventPostProcessingSvc.RegisterHandler(router, cfg)
